db48ab92c4dbb5bde790a84bdea479f3d8118cf7
tjdgns1502 <tjdgns1502@naver.com>
2021-05-12 07:10:11 +0900

    finedust_crawler.py is updated by student2!

--- finedust_crawler.py
+++ finedust_crawler.py
@@ -28,27 +28,46 @@ print(td)
 for content in td:
     print(content.get_text(), end=', ')
     month_ultradust_info.append(content.get_text())
-print("")
 
 month_list=['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월']
 del month_ultradust_info[0]
-month_ultradust_info=[int (i) for i in month_ultradust_info]
+month_ultradust_info=[int(i) for i in month_ultradust_info]
 
 print(month_list)
 print(month_ultradust_info)
+
+finedust_table=soup.find_all('table',{"class":"tableMt"})
+finedust_table_tbody=finedust_table[4].find_all("tbody")
+finedust_table_tbody_row=finedust_table_tbody[0].find_all("tr")
+
+month_finedust_info=[]
+
+td_finedust=finedust_table_tbody_row[0].find_all("td")
+
+for contents in td_finedust:
+    month_finedust_info.append(contents.get_text())
+
+del month_finedust_info[0]
+month_finedust_info= [int (i) for i in month_finedust_info]
+print(month_finedust_info)
 
 matplotlib.rcParams["axes.unicode_minus"]=False
 plt.rc('font',family='Malgun Gothic')
 
 x=range(len(month_list))
 
-fig, ax1=plt.subplots()
-ax1.set_title('월별 초미세먼지/미세먼지 농도 자료')
-ax1.bar(x,month_ultradust_info,color='gray')
-ax1.set_ylim(9,23)
-ax1.set_xlabel("2020년")
-ax1.set_ylabel("농도(단위:㎍/㎥)")
+fig, ax1=plt.subplots(1, 2)
+ax1[0].set_title('월별 초미세먼지 농도 자료')
+ax1[0].bar(x,month_ultradust_info,color='gray')
+ax1[0].set_ylim(9,23)
+ax1[0].set_xlabel("2020년")
+ax1[0].set_ylabel("농도(단위:㎍/㎥)")
 
-ax1.set_xticks(x)
-ax1.set_xticklabels(month_list)
+ax1[1].set_title('월별 미세먼지 농도 자료')
+plt.plot(month_finedust_info)
+
+ax1[0].set_xticks(x)
+ax1[0].set_xticklabels(month_list)
+ax1[1].set_xticks(x)
+ax1[1].set_xticklabels(month_list)
 plt.show()
